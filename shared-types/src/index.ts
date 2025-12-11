188a70a236be137d7e571d4780c595f47b7d54d3
yamachu <yamachu.dev@gmail.com>
2025-12-11 11:27:56 +0900

    Chatで使うmessageのinterface定義する

--- shared-types/src/index.ts
+++ shared-types/src/index.ts
@@ -7,7 +7,15 @@ export interface ToNativeReverseMessage extends ToNativeBaseMessage<"reverse"> {
   text: string;
 }
 
-export type ToNativeMessage = ToNativeReverseMessage | never;
+export interface ToNativeChatMessage extends ToNativeBaseMessage<"chat"> {
+  prompt: string;
+  jwt: string;
+  apiBaseUrl: string;
+  provider: "openai" | "anthropic";
+  model: string;
+}
+
+export type ToNativeMessage = ToNativeReverseMessage | ToNativeChatMessage;
 // #endregion
 
 // #region host to extension
@@ -23,5 +31,48 @@ export type ToExtensionReverseResponse =
   | ToExtensionReverseSuccessResponse
   | ToExtensionErrorResponse;
 
-export type ToExtensionResponse = ToExtensionReverseResponse;
+interface ToExtensionChatBaseResponse<T extends string> {
+  type: T;
+  messageId: string;
+}
+
+export interface ToExtensionChatThinkingResponse
+  extends ToExtensionChatBaseResponse<"thinking"> {}
+
+export interface ToExtensionChatToolCallResponse
+  extends ToExtensionChatBaseResponse<"tool_call"> {
+  toolName: string;
+  toolArgs: string; // JSON stringified arguments
+}
+
+export interface ToExtensionChatToolResultResponse
+  extends ToExtensionChatBaseResponse<"tool_result"> {
+  toolName: string;
+  result: unknown;
+}
+
+export interface ToExtensionChatTokenResponse
+  extends ToExtensionChatBaseResponse<"token"> {
+  token: string;
+}
+
+export interface ToExtensionChatFinalResponse
+  extends ToExtensionChatBaseResponse<"final"> {}
+
+export interface ToExtensionChatErrorResponse
+  extends ToExtensionChatBaseResponse<"error"> {
+  error: string;
+}
+
+export type ToExtensionChatResponse =
+  | ToExtensionChatThinkingResponse
+  | ToExtensionChatToolCallResponse
+  | ToExtensionChatToolResultResponse
+  | ToExtensionChatTokenResponse
+  | ToExtensionChatFinalResponse
+  | ToExtensionChatErrorResponse;
+
+export type ToExtensionResponse =
+  | ToExtensionReverseResponse
+  | ToExtensionChatResponse;
 // #endregion
